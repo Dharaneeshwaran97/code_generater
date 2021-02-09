1ef47cf7a7a8277f61a025da96478672bb1c612f
Dharaneeshwaran97 <58076976+Dharaneeshwaran97@users.noreply.github.com>
2021-02-09 12:11:11 +0530

    add test input json check changes

--- index.js
+++ index.js
@@ -2,148 +2,158 @@
 
 const fs = require('fs');
 
-function getAllTable(data) {
-    let content = "";
-    for (let tableObj of data) {
-        let result = getTableData(tableObj);
-        content += result;
-        content += "\n";
+class SQLGenerater {
+    constructor() {
+
     }
-    return content;
-}
 
-function getTableData(tableObj) {
-    let content = "";
-    let i = 1;
-    content += `create table ${tableObj.tableName} ( \n`
-    for (let fieldObj of tableObj.fields) {
-
-        content += ` ${fieldObj.fieldName}`
-        // content += ` ${tableObj.type}`
-        let dataType = getDataType(fieldObj.type);
-        content += ` ${dataType}`;
-        // console.log("dataType", dataType);
-        content += `(${fieldObj.size})`;
-
-        if (fieldObj.auto_generate == true) {
-            content += " auto_increment "
+    async getAllTable(data) {
+        let content = "";
+        for (let tableObj of data) {
+            let result = await this.getTableData(tableObj);
+            content += result;
+            content += "\n";
         }
-        if (fieldObj.constraints.includes("mandatory")) {
-            content += " not null"
-        }
-        if (fieldObj.default) {
-            if (fieldObj.type == "number") {
-                content += ` default ${fieldObj.default} `
-            } else if (fieldObj.type == "text") {
-                content += ` default '${fieldObj.default}' `
+        return content;
+    }
+
+    async  getTableData(tableObj) {
+        let content = "";
+        let i = 1;
+        content += `create table ${tableObj.tableName} ( \n`
+        for (let fieldObj of tableObj.fields) {
+
+            content += ` ${fieldObj.fieldName}`
+            // content += ` ${tableObj.type}`
+            let dataType = await this.getDataType(fieldObj.type);
+            content += ` ${dataType}`;
+            // console.log("dataType", dataType);
+            content += `(${fieldObj.size})`;
+
+            if (fieldObj.auto_generate == true) {
+                content += " auto_increment "
             }
-        }
+            if (fieldObj.constraints.includes("mandatory")) {
+                content += " not null"
+            }
+            if (fieldObj.default) {
+                if (fieldObj.type == "number") {
+                    content += ` default ${fieldObj.default} `
+                } else if (fieldObj.type == "text") {
+                    content += ` default '${fieldObj.default}' `
+                }
+            }
 
 
-        // if (i != tableObj.fields.length) {
-        content += `,`;
-        // }
-        // i++;
-
-    }
-    content += `\n`;
-    // var result = 
-    content += getConstraintsForAllFields(tableObj.fields);
-    content = content.substr(0, content.length - 2);
-    content += `\n);`
-
-    console.log("content", content);
-    return content;
-    // for (let tableDataValues of tableData){}
-
-}
-
-function getConstraintsForAllFields(fields) {
-    let content = "";
-    console.log("fieldsValues", fields);
-    for (let fieldObj of fields) {
-        // console.log("constraints", values.constraints);
-        let constraintsContent = getContrainsTable(fieldObj);
-        if (constraintsContent.length > 0) {
-            content += constraintsContent;
-            content += ",\n"
+            // if (i != tableObj.fields.length) {
+            content += `,`;
+            // }
+            // i++;
 
         }
+        content += `\n`;
+        // var result = 
+        content += await this.getConstraintsForAllFields(tableObj.fields);
+        content = content.substr(0, content.length - 2);
+        content += `\n);`
+
+        return content;
+        // for (let tableDataValues of tableData){}
 
     }
-    return content;
-}
-function getCheckConstraints(field) {
-    let content = "";
 
-    if (field.minLength) {
-        content += `CHECK(LENGTH(${field.fieldName}) >= ${field.minLength})`
-    }
-    return content;
+    async  getConstraintsForAllFields(fields) {
+        let content = "";
+        // console.log("fieldsValues", fields);
+        for (let fieldObj of fields) {
+            // console.log("constraints", values.constraints);
+            let constraintsContent = await this.getContrainsTable(fieldObj);
+            if (constraintsContent.length > 0) {
+                content += constraintsContent;
+                content += ",\n"
 
-}
-
-function getContrainsTable(fieldObj) {
-    let content = "";
-    console.log("values", fieldObj);
-    let constraints = fieldObj.constraints;
-
-    if (constraints) {
-        for (let constraint of constraints) {
-            switch (constraint) {
-                case "primarykey":
-                    content += `primary key (${fieldObj.fieldName})`;
-                    break;
-                case "unique":
-                    content += `unique (${fieldObj.fieldName})`;
-                    break;
-                case "foreignkey":
-                    content += `foreign key (${fieldObj.fieldName}) references ${fieldObj.references.tableName}(${fieldObj.references.columnName})`
-                    break;
-                case "check":
-                    content += getCheckConstraints(fieldObj);
-                    break;
             }
 
         }
+        return content;
+    }
+    async getCheckConstraints(field) {
+        let content = "";
+
+        if (field.minLength) {
+            content += `CHECK(LENGTH(${field.fieldName}) >= ${field.minLength})`
+        }
+        return content;
 
     }
-    return content;
-}
 
-function getDataType(type) {
-    let dataType = null;
+    async getContrainsTable(fieldObj) {
+        let content = "";
+        // console.log("values", fieldObj);
+        let constraints = fieldObj.constraints;
 
-    switch (type) {
-        case "number":
-            dataType = "int";
-            break;
-        case "text":
-            dataType = "varchar"
-            break;
-        default:
-            dataType = type;
+        if (constraints) {
+            for (let constraint of constraints) {
+                switch (constraint) {
+                    case "primarykey":
+                        content += `primary key (${fieldObj.fieldName})`;
+                        break;
+                    case "unique":
+                        content += `unique (${fieldObj.fieldName})`;
+                        break;
+                    case "foreignkey":
+                        content += `foreign key (${fieldObj.fieldName}) references ${fieldObj.references.tableName}(${fieldObj.references.columnName})`
+                        break;
+                    case "check":
+                        content += await this.getCheckConstraints(fieldObj);
+                        break;
+                }
+
+            }
+
+        }
+        return content;
     }
-    return dataType;
 
-}
+    getDataType(type) {
+        let dataType = null;
+
+        switch (type) {
+            case "number":
+                dataType = "int";
+                break;
+            case "text":
+                dataType = "varchar"
+                break;
+            default:
+                dataType = type;
+        }
+        return dataType;
+
+    }
 
 
-async function getSQLScript(inputData) {
-    let content = getAllTable(inputData);
-    return content;
-}
+    async  getSQLScript(inputData) {
+        let content = await this.getAllTable(inputData);
+        return content;
+    }
 
 
-async function generateSQLScriptFile(inputData) {
-    let content = getAllTable(inputData);
-    fs.writeFile("output.sql", content, function (err, resp) {
-        if (err) throw err;
-        console.log("File updated", resp);
-        return resp;
-    });
+    async  generateSQLScriptFile(inputData) {
+        let content = await this.getAllTable(inputData);
+        fs.writeFile("output.sql", content, function (err, resp) {
+            if (err) throw err;
+            // console.log("File updated", resp);
 
-}
+        });
+
+    }
+
+};
+
+exports.SQLGenerater = SQLGenerater;
+
+
 
 // // let content = getAllTable(data);
 // fs.readFile("input.json", (error, data) => {
